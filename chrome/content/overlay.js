f9db4260c65e7c6e94239c28ac6dd486e5593ee6
mic <m@micz.it>
2013-12-18 21:47:21 +0100

    added prefs usage to the columns showing function

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -3,17 +3,39 @@ if(!it.micz) it.micz={};
 if(!it.micz.ColumnCV) it.micz.ColumnCV={};
 
 it.micz.ColumnCV = {
+
+  showLocation: true,
+  showAccount: false,
+  showAttachment: false,
+
 	init: function() {
 	try{
 		// initialization code
     let tabmail = document.getElementById("tabmail");
-    var monitor = {
+    let monitor = {
       onTabTitleChanged:function(tab){},
       onTabSwitched: function(tab)
       {
+        let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+        prefs = prefs.getBranch("extensions.ColumnCV.");
+        this.showLocation = prefs.getBoolPref("ShowLocation");
+        this.showAccount = prefs.getBoolPref("ShowAccount");
+        this.showAttachment = prefs.getBoolPref("ShowAttachment");
+
     	  if(tab.mode.name=='glodaList'){
+    	  if(this.showLocation){ //show location column
           let loccv = document.getElementById("locationCol");
           if(loccv) loccv.setAttribute("hidden", "false");
+         }
+         if(this.showAccount){ //show account column
+          let accountcv = document.getElementById("accountCol");
+          if(accountcv) accountcv.setAttribute("hidden", "false");
+         }
+         if(this.showAttachment){ //show attachment column
+          let attachcv = document.getElementById("attachmentCol");
+          if(attachcv) attachcv.setAttribute("hidden", "false");
+         }
+
         }
       }
     };
