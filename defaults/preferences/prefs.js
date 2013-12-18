35d5bb0c10e557835a1860483e1eec2d53fd45b5
mic <m@micz.it>
2013-12-18 21:36:04 +0100

    preference panel added

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,1 +1,4 @@
 pref("extensions.ColumnCV@micz.it.description", "chrome://ColumnCV/locale/overlay.properties");
+pref("extensions.ColumnCV.ShowLocation", true);
+pref("extensions.ColumnCV.ShowAccount", false);
+pref("extensions.ColumnCV.ShowAttachment", false);
