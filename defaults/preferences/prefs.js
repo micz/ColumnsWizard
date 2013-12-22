5d6f7d4595ad14f9e897e97906039dec9446e304
mic <m@micz.it>
2013-12-22 14:16:20 +0100

    using localization method only for gecko 19+

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,4 +1,3 @@
-pref("extensions.ColumnsWizard@micz.it.description", "chrome://ColumnsWizard/locale/overlay.properties");
 pref("extensions.ColumnsWizard.ShowLocation", true);
 pref("extensions.ColumnsWizard.ShowAccount", false);
 pref("extensions.ColumnsWizard.ShowAttachment", false);
