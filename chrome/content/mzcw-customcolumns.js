d260dd9c4ff43234bf8d479a84f3821d6f2dbaa2
mic <m@micz.it>
2013-12-29 21:19:56 +0100

    added use strict

--- chrome/content/mzcw-customcolumns.js
+++ chrome/content/mzcw-customcolumns.js
@@ -1,4 +1,4 @@
-if (!miczColumnsWizard) var miczColumnsWizard={};
+"use strict";
 miczColumnsWizard.CustCols={
 	//Cc
   CreateDbObserver_Cc: {
--- chrome/content/mzcw-prefobserver.js
+++ chrome/content/mzcw-prefobserver.js
@@ -1,4 +1,4 @@
-if (!miczColumnsWizard) var miczColumnsWizard={};
+"use strict";
 /**
  * @constructor
  *
