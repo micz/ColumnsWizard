51abde618fff4e06f9a4dc00b4e390e9188526fc
mic <m@micz.it>
2014-05-20 23:25:21 +0200

    cc custcol with dynamic code

--- chrome/content/mzcw-customcolumns.js
+++ chrome/content/mzcw-customcolumns.js
@@ -5,62 +5,55 @@ miczColumnsWizard.CustCols={
     // Components.interfaces.nsIObserver
     observe: function(aMsgFolder, aTopic, aData)
                 {
-                 miczColumnsWizard.CustCols.addCustomColumnHandler_Cc();
+                 miczColumnsWizard.CustCols.addCustomColumnHandler("cc");
                 }
   },
 
-  columnHandler_Cc: {
-     getCellText:         function(row, col) {
-        //get the message's header so that we can extract the cc to field
-        let hdr = gDBView.getMsgHdrAt(row);
-        return hdr.getStringProperty("ccList");
-     },
-     getSortStringForRow: function(hdr) {return hdr.getStringProperty("ccList");},
-     isString:            function() {return true;},
-     getCellProperties:   function(row, col, props){},
-     getRowProperties:    function(row, props){},
-     getImageSrc:         function(row, col) {return null;},
-     getSortLongForRow:   function(hdr) {return 0;}
+  addCustomColumnHandler: function(coltype) {
+     gDBView.addColumnHandler(coltype+"Col_cw", this["columnHandler_"+coltype]);
   },
-  
-  addCustomColumnHandler_Cc: function() {
-     gDBView.addColumnHandler("ccCol_cw", this.columnHandler_Cc);
-  },
-  //Cc - END
   
   addCustomColumn: function(coltype){
     let strBundleCW = Components.classes["@mozilla.org/intl/stringbundle;1"].getService(Components.interfaces.nsIStringBundleService);
     let _bundleCW = strBundleCW.createBundle("chrome://columnswizard/locale/overlay.properties");
 
-    switch(coltype){
-      case "cc":
-          if(document.getElementById("ccCol_cw"))break;
-          let labelString = _bundleCW.GetStringFromName("ColumnsWizardCc.label");
-          let tooltipString = _bundleCW.GetStringFromName("ColumnsWizardCcDesc.label");
-          let ccCol_cw = document.createElement("treecol");
-          ccCol_cw.setAttribute("id","ccCol_cw");
-          ccCol_cw.setAttribute("persist","hidden ordinal width");
-          ccCol_cw.setAttribute("hidden","true");
-          ccCol_cw.setAttribute("flex","4");
-          ccCol_cw.setAttribute("label",labelString);
-          ccCol_cw.setAttribute("tooltiptext",tooltipString);
-          let ccSplitter_cw = document.createElement("splitter");
-          ccSplitter_cw.setAttribute("class","tree-splitter");
-          let element = document.getElementById("threadCols");
-          element.appendChild(ccSplitter_cw);
-          element.appendChild(ccCol_cw);
-        break;
-      default: break;
+    if(document.getElementById(coltype+"Col_cw")){
+      return;
     }
+    let labelString = _bundleCW.GetStringFromName("ColumnsWizard"+coltype+".label");
+    let tooltipString = _bundleCW.GetStringFromName("ColumnsWizard"+coltype+"Desc.label");
+    let cwCol = document.createElement("treecol");
+    cwCol.setAttribute("id",coltype+"Col_cw");
+    cwCol.setAttribute("persist","hidden ordinal width");
+    cwCol.setAttribute("hidden","true");
+    cwCol.setAttribute("flex","4");
+    cwCol.setAttribute("label",labelString);
+    cwCol.setAttribute("tooltiptext",tooltipString);
+    let cwSplitter = document.createElement("splitter");
+    cwSplitter.setAttribute("class","tree-splitter");
+    let element = document.getElementById("threadCols");
+    element.appendChild(cwSplitter);
+    element.appendChild(cwCol);
   },
   
     removeCustomColumn: function(coltype){
-    switch(coltype){
-      case "cc":
-          let element = document.getElementById("ccCol_cw");
-          if(element) element.parentNode.removeChild(element);
-        break;
-      default: break;
-    }    
-  }, 
+      let element = document.getElementById(coltype+"Col_cw");
+      if(element) element.parentNode.removeChild(element);
+    }, 
 };
+
+//cc
+miczColumnsWizard.CustCols["columnHandler_cc"]={
+   getCellText:         function(row, col) {
+      //get the message's header so that we can extract the cc to field
+      let hdr = gDBView.getMsgHdrAt(row);
+      return hdr.getStringProperty("ccList");
+   },
+   getSortStringForRow: function(hdr) {return hdr.getStringProperty("ccList");},
+   isString:            function() {return true;},
+   getCellProperties:   function(row, col, props){},
+   getRowProperties:    function(row, props){},
+   getImageSrc:         function(row, col) {return null;},
+   getSortLongForRow:   function(hdr) {return 0;}
+};
+//cc - END
