4166014a04b5d5222f7a346d5fe7ba1c16674b27
Mic <m@micz.it>
2015-01-17 15:27:38 +0100

    extensions.ColumnsWizard.DefaultColsList.active is now true by default

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -8,4 +8,4 @@ pref("extensions.ColumnsWizard.CustCols.Addreplyto", false);
 pref("extensions.ColumnsWizard.CustCols.Addxoriginalfrom", false);
 pref("extensions.ColumnsWizard.CustCols.Addcontentbase", false);
 pref("extensions.ColumnsWizard.DefaultColsList", "");
-pref("extensions.ColumnsWizard.DefaultColsList.active", false);
+pref("extensions.ColumnsWizard.DefaultColsList.active", true);
