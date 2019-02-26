d5b956214f2f4fbc54f98c76805b2121f6511837
Christopher Leidigh <cleidigh@Gmail.com>
2019-02-26 18:46:08 -0500

    locale scripts 1

--- scripts/dtd-test1.js
+++ scripts/dtd-test1.js
@@ -8,14 +8,14 @@ loadLocales = function (localeDir, referenceLocale, localeIds) {
 	// GetLocaleFiles
 	const localeFolders = _getAllFilesOrFolders(localeDir, true);
 
-	if (localeFolders.indexOf(referenceLocale) === -1 ) {
+	if (localeFolders.indexOf(referenceLocale) === -1) {
 		return [];
 	}
 
 	let locales = [];
 
-	localeFolders.forEach( (localeFolder, index) => {
-		locales.push({localeId: localeFolder, localePath: `${localeDir}/${localeFolder}`, referenceLocale: ((localeFolder === referenceLocale) ? true : false)});
+	localeFolders.forEach((localeFolder, index) => {
+		locales.push({ localeId: localeFolder, localePath: `${localeDir}/${localeFolder}`, referenceLocale: ((localeFolder === referenceLocale) ? true : false) });
 		let localeFiles = _getAllFilesOrFolders(`${localeDir}/${localeFolder}`, false);
 
 		locales[index].localeFiles = localeFiles;
@@ -58,41 +58,44 @@ var _getAllFilesOrFolders = function (dir, foldersOnly) {
 
 };
 
-compareLocaleEntities = function(locales, referenceLocaleId, localeSet) {
-	
+compareLocaleEntities = function (locales, referenceLocaleId, localeSet) {
+
 	// const refIndex = localeFolders.indexOf(referenceLocale);
 
 	const refIndex = 1;
 	if (refIndex === -1) {
 		return 1;
 	}
-	
-	locales.forEach( (locale, index) => {
-		if (index  === refIndex) {
+
+	locales.forEach((locale, index) => {
+		if (index === refIndex) {
 			// Skip reference locale
+			// const results = compareDTDFiles(`${locales[refIndex].localePath}/${file}`, null);
+			// locale.entities = results.entities;
 			return;
 		}
 
-		locales[refIndex].localeFiles.forEach( file => {
+
+		locales[refIndex].localeFiles.forEach(file => {
 			if (locale.localeFiles.indexOf(file) === -1) {
-				console.log(`File Missing (${locale.localeId}) ${file}`);
+				// console.log(`File Missing (${locale.localeId}) ${file}`);
 				locale.missingFiles.push(file);
 				return;
 			} else {
-				if (file.substr(-4).toLowerCase() === '.dtd' ) {
+				if (file.substr(-4).toLowerCase() === '.dtd') {
 					console.log(`Processing DTD: (${locale.localeId}) ${file}`);
-					const results = compareDTDFiles(`${locales[refIndex].localePath}/${file}`,`${locale.localePath}/${file}`);
-					locale.missingEntities = locale.missingEntities.concat(results.missingEntities);
-					locale.extraEntities = locale.extraEntities.concat(results.extraEntities);
-				} else if (file.substr(-11).toLowerCase() === '.properties'   ){
+					const results = compareDTDFiles(`${locales[refIndex].localePath}/${file}`, `${locale.localePath}/${file}`);
+					locale.missingEntities = locale.missingEntities.concat(results.missingEntities).sort();
+					locale.extraEntities = locale.extraEntities.concat(results.extraEntities).sort();
+				} else if (file.substr(-11).toLowerCase() === '.properties') {
 					console.log(`Processing Properties: (${locale.localeId}) ${file}`);
-					const results = comparePropertyFiles(`${locales[refIndex].localePath}/${file}`,`${locale.localePath}/${file}`);
-					locale.missingEntities = locale.missingEntities.concat(results.missingEntities);
-					locale.extraEntities = locale.extraEntities.concat(results.extraEntities);
+					const results = comparePropertyFiles(`${locales[refIndex].localePath}/${file}`, `${locale.localePath}/${file}`);
+					locale.missingEntities = locale.missingEntities.concat(results.missingEntities).sort();
+					locale.extraEntities = locale.extraEntities.concat(results.extraEntities).sort();
 				}
 			}
 		});
-		
+
 	});
 
 
@@ -101,9 +104,9 @@ compareLocaleEntities = function(locales, referenceLocaleId, localeSet) {
 compareDTDFiles = function (referenceFile, comparisonFile) {
 	var parser = require("dtd-file");
 
-	console.log(referenceFile+'  '+comparisonFile);
-	let refEntities =  Object.keys(parser.parse(fs.readFileSync(referenceFile, 'utf-8')));
-	let compEntities =  Object.keys(parser.parse(fs.readFileSync(comparisonFile, 'utf-8')));
+	// console.log(referenceFile+'  '+comparisonFile);
+	let refEntities = Object.keys(parser.parse(fs.readFileSync(referenceFile, 'utf-8')));
+	let compEntities = Object.keys(parser.parse(fs.readFileSync(comparisonFile, 'utf-8')));
 
 	let missingEntities = [];
 
@@ -126,17 +129,17 @@ compareDTDFiles = function (referenceFile, comparisonFile) {
 
 comparePropertyFiles = function (referenceFile, comparisonFile) {
 
-	console.log(referenceFile+'  '+comparisonFile);
-	let refProperties =  fs.readFileSync(referenceFile, 'utf-8').split('\n').map(l => l.split('=')[0]);
+	console.log(referenceFile + '  ' + comparisonFile);
+	let refProperties = fs.readFileSync(referenceFile, 'utf-8').split('\n').map(l => l.split('=')[0]);
 	let compProperties = fs.readFileSync(comparisonFile, 'utf-8').split('\n').map(l => l.split('=')[0]);;
 
-	console.log(compProperties);
+	// console.log(compProperties);
 
 	let missingEntities = [];
 
 	refProperties.forEach(property => {
 		// const property = propertyLine.split('=')[0];
-		
+
 		// console.log('Property: '+ property);
 
 		const i = compProperties.indexOf(property);
@@ -208,3 +211,72 @@ const localeSet = null;
 // let result = compareDTDFiles(`${locales[1].localePath}/${locales[1].localeFiles[0]}`, `${locales[4].localePath}/${locales[4].localeFiles[0]}`);
 let result = compareLocaleEntities(locales);
 console.log(locales);
+
+localeReport = function (referenceLocaleId, compLocaleId, details) {
+
+	let compLocaleIndex = -1;
+
+	console.log('Reference Locale:' + referenceLocaleId);
+
+	locales.forEach((locale, index) => {
+		if (locale.localeId === compLocaleId) {
+			compLocaleIndex = index;
+			return;
+		}
+	});
+
+	const compLocale = locales[compLocaleIndex];
+	console.log('Locale: ' + compLocaleId + '\n');
+
+	if (compLocale.missingFiles.length) {
+		console.log('  Missing Files:');
+		compLocale.missingFiles.forEach(file => {
+			console.log('    ' + file);
+		});
+
+	}
+
+	if (compLocale.missingEntities.length) {
+
+		let file = '';
+		let missingEntitySet = new Object();
+
+		console.log('  Missing Entities:');
+		compLocale.missingEntities.forEach(entityLine => {
+			entityLineElements = entityLine.split('$');
+
+			if (locale[referenceLocaleId]) {
+				
+			}
+			missingEntitySet[`${entityLineElements[1]}`] = refEntity;
+
+			if (file !== entityLineElements[0]) {
+				file = entityLineElements[0];
+				console.log(`\n   File:  ${entityLineElements[0]}`);	
+				if (file !=='') {
+					writeMissingEntitiesFile(`${file}.missing`, missingEntitySet);
+					missingEntitySet = {};
+				}
+			}
+			console.log(    `          ${entityLineElements[1]}`);
+		});
+	}
+};
+
+writeMissingEntitiesFile = function(file, entities) {
+	var parser = require("dtd-file");
+	let fileData = '';
+
+	fileData = parser.stringify(entities);
+	console.log('\n'+ fileData );
+
+	fs.writeFile(file, fileData, function(err) {
+		if(err) {
+			return console.log(err);
+		}
+	
+		console.log("The file was saved!");
+	})
+};
+
+localeReport(referenceLocaleId, 'zh-CN');
